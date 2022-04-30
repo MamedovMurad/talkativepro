e70d1d607e7c92a51b3d03004b5921c6cb3497f7
Murad Məmmədov <muradmemmedov955@gmail.con>
2022-04-30 23:33:25 +0400

    fixed css bugs on checkbox bgcolor

--- components/UI/Checkbox/index.tsx
+++ components/UI/Checkbox/index.tsx
@@ -5,22 +5,45 @@ type CheckBoxProps = {
   text: string;
   id: string;
 };
-
+const delay = 1;
 const CheckBox: React.FC<CheckBoxProps> = ({ id, text }) => {
-  const [first, setfirst] = useState(false)
-  useEffect(() => {
-setfirst(!first)
-  }, [])
-  
+  const [first, setfirst] = useState(false);
+  useEffect(
+    () => {
+      let timer1 = setTimeout(() => setfirst(true), delay );
+
+      // this will clear Timeout
+      // when component unmount like in willComponentUnmount
+      // and show will not change to true
+      console.log('fafsdafsda');
+      
+      return () => {
+        clearTimeout(timer1);
+      };
+    },
+    // useEffect will run only one time with empty []
+    // if you pass a value to array,
+    // like this - [data]
+    // than clearTimeout will run every time
+    // this value changes (useEffect re-run)
+    []
+  );
+
   return (
     <>
-  {
-    first?  <label htmlFor={"customCHeckBox-" + id} className={styles.customcheckbox}>
-    <input type="checkbox" name={text} id={"customCHeckBox-" + id}/>
-    <span className={styles.checkmark}></span>
-    <span className={styles.label}>{text}</span>
-  </label>: 'loading'
-  }
+     
+        <label
+          htmlFor={"customCHeckBox-" + id}
+          className={styles.customcheckbox}
+        >
+          <input type="checkbox" name={text} id={"customCHeckBox-" + id} />
+          <span
+            className={styles.checkmark}
+            style={{ backgroundColor: first ? "#00C1DD" : "white" }}
+          ></span>
+          <span className={styles.label}>{text}</span>
+        </label>
+ 
     </>
   );
 };
