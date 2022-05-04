24be544008eb1bcb32f711b562e70ce296b84766
Murad Məmmədov <muradmemmedov955@gmail.con>
2022-05-04 11:32:17 +0400

    add router active

--- components/main_nav/index.tsx
+++ components/main_nav/index.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import { nav } from '../../Model/utils/nav';
-import styles from './index.module.css'
-type MainNavProps = {}
- 
-const MainNav:React.FC<MainNavProps> = () => {
-    return (
-        <ul className={styles.nav}>
-        {
-            nav.map(item=>(
-                <li key={item.id}><Link href={item.path}>
-             <a > {item['title_AZ']}</a>
-              </Link></li>
-            ))
-        }
-        
+import Link from "next/link";
+import { nav } from "../../Model/utils/nav";
+import styles from "./index.module.css";
+import { useRouter } from 'next/router'
+type MainNavProps = {};
+
+const MainNav: React.FC<MainNavProps> = () => {
+    const router = useRouter()
+  return (
+    <ul className={styles.nav}>
+      {nav.map((item) => (
+        <li key={item.id} className={router.pathname==item.path?styles.activeNav:styles.nafv}>
+          <Link href={item.path}>
+            <a> {item["title_AZ"]}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
-    );
-}
- 
- 
+  );
+};
+
 export default MainNav;
